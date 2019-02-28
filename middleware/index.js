3352314af0bdc037915c79faba9046fcee4b53e6
Jordan Fear <jordanfear1989@gmail.com>
2019-02-28 19:28:15 +0000

    back to my middleware comment

--- middleware/index.js
+++ middleware/index.js
@@ -26,46 +26,46 @@ middlewareObj.checkCampOwnership = function(req, res, next) {
     }
 }
 
-// middlewareObj.checkCommentOwnership = (req, res, next) => {
-//     if (req.isAuthenticated()) {
-//         Comment.findById(req.params.comments_id, function(err, foundComment) {
-//             if (err) {
-//                 res.redirect("back");
-//             } else {
-//                 // does user own the comment?
-//                 if (foundComment.author.id.equals(req.user._id)) {
-//                     next();
-//                 } else {
-//                     req.flash('error', 'You didn\'t create the comment')
-//                     res.redirect("back");
-//                 }
-//             }
-//         });
-//     } else {
-//         res.redirect("back");
-//     }
-// };
-
-middlewareObj.checkCommentOwnership = function(req, res, next) {
- if(req.isAuthenticated()){
-        Comment.findById(req.params.comment_id, function(err, foundComment){
-           if(err){
-               res.redirect("back");
-           }  else {
-               // does user own the comment?
-            if(foundComment.author.id.equals(req.user._id)) {
-                next();
-            } else {
-                req.flash("error", "You don't have permission to do that");
+middlewareObj.checkCommentOwnership = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comments_id, function(err, foundComment) {
+            if (err) {
                 res.redirect("back");
+            } else {
+                // does user own the comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    req.flash('error', 'You didn\'t create the comment')
+                    res.redirect("back");
+                }
             }
-           }
         });
     } else {
-        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
-}
+};
+
+// middlewareObj.checkCommentOwnership = function(req, res, next) {
+//  if(req.isAuthenticated()){
+//         Comment.findById(req.params.comment_id, function(err, foundComment){
+//           if(err){
+//               res.redirect("back");
+//           }  else {
+//               // does user own the comment?
+//             if(foundComment.author.id.equals(req.user._id)) {
+//                 next();
+//             } else {
+//                 req.flash("error", "You don't have permission to do that");
+//                 res.redirect("back");
+//             }
+//           }
+//         });
+//     } else {
+//         req.flash("error", "You need to be logged in to do that");
+//         res.redirect("back");
+//     }
+// }
 
 
 
